89253bff4bdfad8b55b8aab672d179d2ea8409b0
barared28 <49855426+barared28@users.noreply.github.com>
2020-01-25 02:58:23 +0000

    Add files via upload

---
+++ no1.js
@@ -0,0 +1,10 @@
+var burung = 6969;
+var a =  441/21;
+
+
+for (let i = 0; i < a;i++){
+    burung = burung - (burung * (11/100));
+    burung = burung + burung
+}
+
+console.log('Jumlah Lovebird Setelah 441 hari adalah '+burung+' ekor')
